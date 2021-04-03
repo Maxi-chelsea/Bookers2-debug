58561a573664a9e44f09b5bb5238bba9b724c38d
Maxi-chelsea <maxi19950049@gmail.com>
2021-04-03 15:16:57 +0000

    [First Commit]

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'homes#top'
   get "home/about" => "homes#about" , as: "about"
   devise_for :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
 end
